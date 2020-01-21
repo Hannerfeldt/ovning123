59d1ca339a531b923fa3901ca8f12b131ca8ceea
Jonathan <Hannerfeldt@hotmail.se>
2020-01-21 13:47:06 +0100

    hsdsaaghs<2

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ console.log("tarmspräckarelof");
 console.log("hej från stefan");
 
 console.log("tarmspräckarelof");
+
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("hello");
+
+})
+
+app.listen(5000, ()=> { console.log("server live")});
